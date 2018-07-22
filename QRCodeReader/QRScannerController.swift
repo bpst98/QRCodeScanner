b35aeca84d9fdb85466d488a1605a810e7260ac4
Bhanu Thapliyal <bpst98@gmail.com>
2018-07-22 22:24:32 +0530

    ..

--- QRCodeReader/QRScannerController.swift
+++ QRCodeReader/QRScannerController.swift
@@ -24,16 +24,22 @@ class QRScannerController: UIViewController ,AVCaptureMetadataOutputObjectsDeleg
 //Finding camera within the device
         
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
-        guard let captureDevice = deviceDiscoverySession.devices.first
-            else{
-                print("Camera Not detetced")
-                return}
         
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera device")
+            return
         }
-        catch{
-            print("Error Not RECOGNIZING")
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Set the input device on the capture session.
+            captureSession?.addInput(input)
+            
+        }
+        catch {
+           
+            print(error)
             return
         }
         
@@ -56,8 +62,39 @@ class QRScannerController: UIViewController ,AVCaptureMetadataOutputObjectsDeleg
         
         captureSession?.startRunning()  //--start video capture
         
+        view.bringSubview(toFront: messageLabel)
+        view.bringSubview(toFront: topbar)
         
+        QRCodeFrameView = UIView()
         
+        if let qrCodeFrameView = QRCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
+        
+        func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+            // Check if the metadataObjects array is not nil and it contains at least one object.
+            if metadataObjects.count == 0 {
+                QRCodeFrameView?.frame = CGRect.zero
+                messageLabel.text = "No QR code is detected"
+                return
+            }
+            
+            // Get the metadata object.
+            let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            
+            if metadataObj.type == AVMetadataObject.ObjectType.qr {
+                // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+                let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+                QRCodeFrameView?.frame = barCodeObject!.bounds
+                
+                if metadataObj.stringValue != nil {
+                    messageLabel.text = metadataObj.stringValue
+                }
+            }
+        }
         
         
         
